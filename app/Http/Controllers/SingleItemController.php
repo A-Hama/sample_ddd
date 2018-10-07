745a1f67bcb7865a3a2dbc3b0df2662af33fc5e8
浜野秋仁 <s.sakae3354@gmail.com>
2018-10-07 17:30:34 +0900

    ルーティングとそれに関わるコントローラーの修正

--- app/Http/Controllers/SingleItemController.php
+++ app/Http/Controllers/SingleItemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SingleItem;
 
 class SingleItemController extends Controller
 {
@@ -13,6 +14,6 @@ class SingleItemController extends Controller
 
     public function create()
     {
-      
+
     }
 }
