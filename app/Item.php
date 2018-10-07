e7b2acca05ac650e6a6b77ba4d88eb659c632b50
浜野秋仁 <s.sakae3354@gmail.com>
2018-10-07 16:07:25 +0900

    singleitem周りの実装

--- app/Item.php
+++ app/Item.php
@@ -10,4 +10,9 @@ class Item extends Model
     {
       return $this->belongsTo('App\Shop');
     }
+
+    public function singleItem()
+    {
+      return $this->hasMany('App\SingleItem');
+    }
 }
